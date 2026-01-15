f8064d5158abf5794bbd25f559da6e6a197b55ca
RaulFlo123 <raulf@seoscholars.org>
2026-01-15 12:04:08 -0500

    Add files via upload

--- design.js
+++ design.js
@@ -1,103 +1,39 @@
-console.log("js console");
-var grid = document.getElementById("grid-container");
+console.log("js console"); // [__D__] - 1
 
-var xhttp = new XMLHttpRequest();
-const playlist = document.getElementById("playlist");
-const searchInput = document.getElementById("search");
+let data; // [__N__] - 2
+let grid = document.querySelector(".grid-container"); // [__H__] - 3
 
-var tracks = [];
+var xhttp = new XMLHttpRequest(); // [__E__] - 4
 
-xhttp.open("GET", "musicdata.json", true);
-xhttp.send();
+xhttp.onreadystatechange = function() { // [__O__] - 5
+    if (this.readyState == 4 && this.status == 200) { // [__J__] - 6
 
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    let data = JSON.parse(xhttp.responseText);
-    tracks = data.tracks;
+       data = JSON.parse(xhttp.responseText); // [__F__] - 7
+       console.log(data); // [__C__] - 8
 
-    console.log(tracks[0].title);
-    displayTracks(tracks);
-  }
-};
-xhttp.open("GET", "playlist.json", true);
-xhttp.send();
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    let data = JSON.parse(xhttp.responseText);
-    tracks = data.tracks;
+       data.forEach(function(song) { // [__A__] - 9
+        let card = document.createElement("div"); // [__P__] - 10
+        card.classList.add("card"); // [__G__] - 11
 
-    console.log(tracks[0].title);
-    displayTracks(tracks);
-  }
-    console.log(data);
-    displayData(data);
-  
-  data.forEach (function(song) {
-    let card = document.createElement("div");
-    card.classList.add("card");
-    let title = document.createElement("h3");
-    title.textContent = song.title;
-    card.appendChild(title);
-    grid.appendChild(card);
-    
-    let textData =
-  "<div class='song-title'>" + song.title + "</div>" +
-  "<span>" +
-    "Artist: " + song.artist + "<br>" +
-    "Release date: " + song.releasedate + "<br>" +
-    "Needs Research: " + "<br>" +
-    "Duration: " + song.duration + "<br>" +
-    "Artwork: " + song.artwork +
-  "</span>";
+        let textData =
+          "<div class='song-title'>" + song.title + "</div>" +
+          "<span>" +
+          "Publisher: " + song.artist + "<br>" +
+          " Release Date: " + song.releasedate + "<br>" +
+          "Album: " + 
+          "</span>"; // [__K__] - 12
 
+        card.innerHTML = textData; // [__L__] - 13
 
-    card.innerHTML = textData;
+        if (song.imgSrc) { // [__Q__] - 14
+            card.style.backgroundImage = "url('" + song.imgSrc + "')";
+        }
 
-    if(song.image_url){
-        card.style.backgroundImage = "url('" + song.image_url + "')";
-        card.style.backgroundSize = "cover";
-        card.style.backgroundPosition = "center";
+        grid.appendChild(card); 
+       });
+
     }
-    grid.appendChild(card);
-  });
-
-function displayTracks(tracks) {
-  playlist.innerHTML = "";
-
-  tracks.forEach(function(track) {
-    let trackItem = document.createElement("div");
-    trackItem.classList.add("track-item");
-
-    let trackInfo = document.createElement("div");
-    trackInfo.classList.add("track-info");
-
-    let title = document.createElement("h3");
-    title.textContent = track.title;
-    trackInfo.appendChild(title);
-
-    let artist = document.createElement("p");
-    artist.textContent = "Artist: " + track.artist;
-    trackInfo.appendChild(artist);
-
-    let releaseDate = document.createElement("p");
-    releaseDate.textContent = "Release Date: " + track.releasedate;
-    trackInfo.appendChild(releaseDate);
-
-    let duration = document.createElement("p");
-    duration.textContent = "Duration: " + track.duration;
-    trackInfo.appendChild(duration);
-
-    trackItem.appendChild(trackInfo);
-    playlist.appendChild(trackItem);
-  });
-}
-
-searchInput.addEventListener("input", function() {
-  let query = searchInput.value.toLowerCase();
-  let filteredTracks = tracks.filter(function(track) {
-    return track.title.toLowerCase().includes(query) || track.artist.toLowerCase().includes(query);
-  });
-  displayTracks(filteredTracks);
-});
 };
 
+xhttp.open("GET", "data.json", true); 
+xhttp.send(); 
